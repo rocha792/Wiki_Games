839104662cf6055c8bae7766381eb09ad9f836e0
edgarocha <edgarocha792@gmail.com>
2022-04-30 22:37:08 -0600

    Templat 1.0

--- Wiki_Games/routes/web.php
+++ Wiki_Games/routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/Inicio', function () {
+    echo("Hola Estas en Wiki Games");
+});
