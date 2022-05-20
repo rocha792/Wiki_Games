cda93af8377959bfa0fccd35216c8c14d659ab71
edgarocha <edgarocha792@gmail.com>
2022-05-20 17:34:49 -0600

    jas

--- Wiki_Games/resources/views/dashboard/index.blade.php
+++ Wiki_Games/resources/views/dashboard/index.blade.php
@@ -23,26 +23,25 @@
   <title>
     Material Dashboard 2 by Creative Tim
   </title>
+
   <!--     Fonts and icons     -->
-
-
   <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" />
+  
+  
   <!-- Nucleo Icons -->
-
-
   <link href="{{ asset('/Dash/dashboard/css/nucleo-icons.css')}} rel="stylesheet" />
   <link href="{{ asset('/Dash/dashboard/css/nucleo-svg.css')}} rel="stylesheet" />
+  
+  
   <!-- Font Awesome Icons -->
-
-
   <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+  
+  
   <!-- Material Icons -->
-
-
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
+  
+  
   <!-- CSS Files -->
-
-
   <link id="pagestyle" href="{{ asset('/Dash/dashboard/css/material-dashboard.css?v=3.0.2')}} rel="stylesheet" />
 </head>
 
