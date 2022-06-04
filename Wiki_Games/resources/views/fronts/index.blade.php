b43cfa3191f26997e21d4e4928ed94bf5c37bafc
edgarocha <edgarocha792@gmail.com>
2022-06-03 23:49:41 -0600

    Avance 2.1

--- Wiki_Games/resources/views/fronts/index.blade.php
+++ Wiki_Games/resources/views/fronts/index.blade.php
@@ -6,57 +6,66 @@
                 <div class="col-lg-8">
                     <!-- Featured blog post-->
                     <div class="card mb-4">
-                        <a href="#!"><img class="card-img-top" src="https://dummyimage.com/850x350/dee2e6/6c757d.jpg" alt="..." /></a>
+                        <a href="#!"><img class="card-img-top" src="./img/Genshin Impact.jpg" alt="..." /></a>
                         <div class="card-body">
-                            <div class="small text-muted">January 1, 2022</div>
-                            <h2 class="card-title">Featured Post Title</h2>
-                            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!</p>
-                            <a class="btn btn-primary" href="#!">Read more →</a>
+                            <div class="small text-muted">Aventura, Rol</div>
+                            <h2 class="card-title">Genshin Impact</h2>
+                            <p class="card-text">Genshin Impact es un videojuego de rol de acción de mundo abierto y gratuito, 
+                                con una mecánica de monetización de Gacha para conseguir elementos adicionales como personajes especiales y armas.</p>
+                            <a class="btn btn-primary" href="#!">Leer más →</a>
                         </div>
                     </div>
                     <!-- Nested row for non-featured blog posts-->
                     <div class="row">
                         <div class="col-lg-6">
-                            <!-- Blog post-->
+                            <!-- Dark Souls -->
                             <div class="card mb-4">
-                                <a href="#!"><img class="card-img-top" src="https://dummyimage.com/700x350/dee2e6/6c757d.jpg" alt="..." /></a>
+                                <a href="#!"><img class="card-img-top" src="./img/Dark Souls.jpg" alt="..." /></a>
                                 <div class="card-body">
-                                    <div class="small text-muted">January 1, 2022</div>
-                                    <h2 class="card-title h4">Post Title</h2>
-                                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla.</p>
-                                    <a class="btn btn-primary" href="#!">Read more →</a>
+                                    <div class="small text-muted">Accion, Aventura</div>
+                                    <h2 class="card-title h4">Dark Souls</h2>
+                                    <p class="card-text">‘Dark Souls: Prepare to Die Edition’, el juego de PC, es exactamente igual 
+                                        al ‘Dark Souls’ de consola, pero con unos cuantos contenidos extra: dos nuevos jefes finales, 
+                                        un nuevo capítulo, nuevas zonas explorables, nuevos personajes no controlables y nuevos ítems (armaduras, etc.).</p>
+                                    <a class="btn btn-primary" href="#!">Leer más →</a>
                                 </div>
                             </div>
-                            <!-- Blog post-->
+                            <!-- Super Smash Bros Ultimate -->
                             <div class="card mb-4">
-                                <a href="#!"><img class="card-img-top" src="https://dummyimage.com/700x350/dee2e6/6c757d.jpg" alt="..." /></a>
+                                <a href="#!"><img class="card-img-top" src="./img/Call of Duty.jpg" alt="..." /></a>
                                 <div class="card-body">
-                                    <div class="small text-muted">January 1, 2022</div>
-                                    <h2 class="card-title h4">Post Title</h2>
-                                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla.</p>
-                                    <a class="btn btn-primary" href="#!">Read more →</a>
+                                    <div class="small text-muted">Battle Royal</div>
+                                    <h2 class="card-title h4">Call of Duty</h2>
+                                    <p class="card-text">Call of Duty: Mobile es un videojuego de disparos en primera persona desarrollado 
+                                        por Activision y TiMi Studios de Tencent Games para la plataforma de Android e IOS. Fue lanzado el 
+                                        1 de octubre de 2019. En su primer mes el juego tuvo más de 148 millones de descargas y generó casi 
+                                        US$ 54 millones en ingresos.​​</p>
+                                    <a class="btn btn-primary" href="#!">Leer más →</a>
                                 </div>
                             </div>
                         </div>
                         <div class="col-lg-6">
-                            <!-- Blog post-->
+                            <!-- Call of Duty -->
                             <div class="card mb-4">
-                                <a href="#!"><img class="card-img-top" src="https://dummyimage.com/700x350/dee2e6/6c757d.jpg" alt="..." /></a>
+                                <a href="#!"><img class="card-img-top" src="./img/Super Smash Bros Ultimate.jpg" alt="..." /></a>
                                 <div class="card-body">
-                                    <div class="small text-muted">January 1, 2022</div>
-                                    <h2 class="card-title h4">Post Title</h2>
-                                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla.</p>
-                                    <a class="btn btn-primary" href="#!">Read more →</a>
+                                    <div class="small text-muted">Pelea</div>
+                                    <h2 class="card-title h4">Super Smash Bros Ultimate</h2>
+                                    <p class="card-text">Super Smash Bros. Ultimate es un videojuego de lucha desarrollado por Sora Ltd. 
+                                        y Bandai Namco Entertainment y distribuido por Nintendo para la consola Nintendo Switch. Fue lanzado 
+                                        a nivel mundial el 7 de diciembre de 2018 y se trata del quinto título de la serie Super Smash Bros.</p>
+                                    <a class="btn btn-primary" href="#!">Leer más →</a>
                                 </div>
                             </div>
-                            <!-- Blog post-->
+                            <!-- Free Fire -->
                             <div class="card mb-4">
-                                <a href="#!"><img class="card-img-top" src="https://dummyimage.com/700x350/dee2e6/6c757d.jpg" alt="..." /></a>
+                                <a href="#!"><img class="card-img-top" src="./img/Free Fire.jpg" alt="..." /></a>
                                 <div class="card-body">
-                                    <div class="small text-muted">January 1, 2022</div>
-                                    <h2 class="card-title h4">Post Title</h2>
-                                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam.</p>
-                                    <a class="btn btn-primary" href="#!">Read more →</a>
+                                    <div class="small text-muted">Battle Royal</div>
+                                    <h2 class="card-title h4">Free Fire</h2>
+                                    <p class="card-text">Garena Free Fire es un videojuego battle royale, desarrollado por 111dots studio​ y 
+                                        publicado por Garena para Android e IOS.​​Se convirtió en el juego móvil más descargado a nivel global en 2019.​ </p>
+                                    <a class="btn btn-primary" href="#!">Leer más →</a>
                                 </div>
                             </div>
                         </div>
@@ -94,7 +103,7 @@
                             <div class="row">
                                 <div class="col-sm-6">
                                     <ul class="list-unstyled mb-0">
-                                        <li><a href="#!">Web Design</a></li>
+                                        <li><a href="#!">Admin</a></li>
                                         <li><a href="#!">HTML</a></li>
                                         <li><a href="#!">Freebies</a></li>
                                     </ul>
@@ -111,8 +120,8 @@
                     </div>
                     <!-- Side widget-->
                     <div class="card mb-4">
-                        <div class="card-header">Side Widget</div>
-                        <div class="card-body">You can put anything you want inside of these side widgets. They are easy to use, and feature the Bootstrap 5 card component!</div>
+                        <div class="card-header">Wiki Games</div>
+                        <div class="card-body">La paguina web adecuada para saber más sobre tus juegos competitivos</div>
                     </div>
                 </div>
             </div>
